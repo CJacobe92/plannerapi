1a9968d124ceb0e0d6de7d9ddbc06b88883cd9d0
CJacobe92 <jacobe.christian92@gmail.com>
2023-07-29 04:00:20 +0800

    fixed_mailer_url

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,7 @@
 class UserMailer < ApplicationMailer
   def password_reset_email(user, token)
     @user = user
-    # @frontend_url = Rails.application.credentials.dig(:frontend, :url)
-    @reset_url= "https://plannerapi.onrender.com/api/v1/password_reset?token=#{token}"
+    @reset_url= "https://taskedcjacobe.netlify.app/api/v1/password_reset?token=#{token}"
     mail(to: @user.email, subject: 'Password Reset')
   end
 end
